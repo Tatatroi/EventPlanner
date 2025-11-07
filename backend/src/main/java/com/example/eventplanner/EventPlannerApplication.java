25d43926513b8cf829c3084fd78337d9b20fc37d
Tatatroi <srefanmitroi@gmail.com>
2025-11-07 12:39:54 +0200

    Model Creation (Change names and default port 8080 to 8081)

--- backend/src/main/java/com/example/eventplanner/EventPlannerApplication.java
+++ backend/src/main/java/com/example/eventplanner/EventPlannerApplication.java
@@ -1,9 +1,12 @@
 package com.example.eventplanner;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.eventplanner.model")
 public class EventPlannerApplication {
 
 	public static void main(String[] args) {
